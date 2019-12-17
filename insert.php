9ab3826c4723c06451b629732ea24b03e51a20c9
Don Moses <githinjidon@gmail.com>
2019-12-17 14:36:40 +0300

    add telephone attribute and make minor fixes

--- insert.php
+++ insert.php
@@ -5,13 +5,14 @@ require_once "phpmysqlconnect.php"
 $full_name = mysqli_real_escape_string($conn, $_REQUEST['full_name']);
 $user_name = mysqli_real_escape_string($conn, $_REQUEST['user_name']);
 $email = mysqli_real_escape_string($conn, $_REQUEST['email']);
-$password = mysqli_real_escape_string($conn, $_REQUEST['password'])
+$password = mysqli_real_escape_string($conn, $_REQUEST['password']);
+$telephone = mysqli_real_escape_string($conn, $_REQUEST['telephone'])
 // Attempt insert query execution
-$sql = "INSERT INTO persons (full_name, user_name, password, email) VALUES ('$full_name', '$user_name','$password', '$email')";
+$sql = "INSERT INTO userinfo (full_name, user_name, telephone, password, email) VALUES ('$full_name', '$user_name','$telephone''$password', '$email')";
 if(mysqli_query($conn, $sql)){
     echo "Records added successfully.";
 } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 }
  
 // Close connection
